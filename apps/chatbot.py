6018c3baebf76cf5fad06b12cf46d9ecdb7d62e4
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-02-22 06:06:30 +0100

    replace Argparse by Fire (#21)

--- apps/chatbot.py
+++ apps/chatbot.py
@@ -1,30 +1,58 @@
 """Run a chatbot with FlexGen and OPT models."""
-import argparse
+from typing import Tuple
 
+import fire as fire
 from transformers import AutoTokenizer
-from flexgen.flex_opt import (Policy, OptLM, TorchDevice, TorchDisk, TorchMixedDevice,
-    CompressionConfig, Env, Task, get_opt_config)
+from flexgen.flex_opt import (
+    Policy, OptLM, TorchDevice, TorchDisk, TorchMixedDevice, CompressionConfig, Env, get_opt_config
+)
 
 
-def main(args):
+def main(
+    model: str = "facebook/opt-6.7b",
+    path: str = "~/opt_weights",
+    offload_dir: str = "~/flexgen_offload_dir",
+    percent: Tuple[int, int, int, int, int, int] = (100, 0, 100, 0, 100, 0),
+    compress_weight: bool = False,
+    compress_cache: bool = False,
+):
+    """The main entry point.
+
+    Args:
+        model: The model name.
+        path: The path to the model weights. If there are no cached weights,
+            FlexGen will automatically download them from HuggingFace.
+        offload_dir: The directory to offload tensors.
+        percent: Six numbers. They are:
+
+            + the percentage of weight on GPU
+            + the percentage of weight on CPU
+            + the percentage of attention cache on GPU
+            + the percentage of attention cache on CPU
+            + the percentage of activations on GPU
+            + the percentage of activations on CPU
+
+        compress_weight: Whether to compress weight.
+        compress_cache: Whether to compress cache.
+    """
     # Initialize environment
     gpu = TorchDevice("cuda:0")
     cpu = TorchDevice("cpu")
-    disk = TorchDisk(args.offload_dir)
+    disk = TorchDisk(offload_dir)
     env = Env(gpu=gpu, cpu=cpu, disk=disk, mixed=TorchMixedDevice([gpu, cpu, disk]))
 
     # Offloading policy
     policy = Policy(1, 1,
-                    args.percent[0], args.percent[1],
-                    args.percent[2], args.percent[3],
-                    args.percent[4], args.percent[5],
+                    percent[0], percent[1],
+                    percent[2], percent[3],
+                    percent[4], percent[5],
                     overlap=True, sep_layer=True, pin_weight=True,
                     cpu_cache_compute=False, attn_sparsity=1.0,
-                    compress_weight=args.compress_weight,
+                    compress_weight=compress_weight,
                     comp_weight_config=CompressionConfig(
                         num_bits=4, group_size=64,
                         group_dim=0, symmetric=False),
-                    compress_cache=args.compress_cache,
+                    compress_cache=compress_cache,
                     comp_cache_config=CompressionConfig(
                         num_bits=4, group_size=64,
                         group_dim=2, symmetric=False))
@@ -35,8 +63,8 @@ def main(args):
     stop = tokenizer("\n").input_ids[0]
 
     print("Initialize...")
-    opt_config = get_opt_config(args.model)
-    model = OptLM(opt_config, env, args.path, policy)
+    opt_config = get_opt_config(model)
+    model = OptLM(opt_config, env, path, policy)
     model.init_all_weights()
 
     context = (
@@ -82,29 +110,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="facebook/opt-6.7b",
-        help="The model name.")
-    parser.add_argument("--path", type=str, default="~/opt_weights",
-        help="The path to the model weights. If there are no cached weights, "
-             "FlexGen will automatically download them from HuggingFace.")
-    parser.add_argument("--offload-dir", type=str, default="~/flexgen_offload_dir",
-        help="The directory to offload tensors. ")
-    parser.add_argument("--percent", nargs="+", type=int,
-        default=[100, 0, 100, 0, 100, 0],
-        help="Six numbers. They are "
-         "the percentage of weight on GPU, "
-         "the percentage of weight on CPU, "
-         "the percentage of attention cache on GPU, "
-         "the percentage of attention cache on CPU, "
-         "the percentage of activations on GPU, "
-         "the percentage of activations on CPU")
-    parser.add_argument("--compress-weight", action="store_true",
-        help="Whether to compress weight.")
-    parser.add_argument("--compress-cache", action="store_true",
-        help="Whether to compress cache.")
-    args = parser.parse_args()
-
-    assert len(args.percent) == 6
-
-    main(args)
+    fire.Fire(main)
