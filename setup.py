6f8bf5780e1f7850d88009cb31009d0103b16b63
Ying Sheng <sqy1415@gmail.com>
2023-02-21 21:17:15 -0800

    Revert "replace Argparse by Fire (#21)" (#37)
    This reverts commit 6018c3baebf76cf5fad06b12cf46d9ecdb7d62e4.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    "numpy", "tqdm", "pulp", "attrs", "fire",
+    "numpy", "tqdm", "pulp", "attrs",
     "transformers>=4.24", "torch>=1.12",
 ]
 
