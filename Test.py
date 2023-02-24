fd5795774ea6a21a2e00d5560b137ec3ca3cbb7b
Bazla24 <98683451+Bazla24@users.noreply.github.com>
2023-02-24 19:09:18 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ from enum import Enum, auto
 from flexgen.utils import ValueHolder
 
 def test_value():
-    value = ValueHolder(50)
+    value = ValueHolder()
     value.store(50)
     assert wallet.val == 50
 		
