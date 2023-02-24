b8de38831b3344c878cd2ad011564c87f4787841
Bazla24 <98683451+Bazla24@users.noreply.github.com>
2023-02-24 16:52:02 +0500

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,7 @@
+from enum import Enum, auto
+from pytorch_backend import DeviceType
+import unittest
+
+class MyTestCase(unittest.TestCase):
+	def test_convert(self):
+		self.assertEqual(DeviceType.convert("cuda"), DeviceType.CUDA)
